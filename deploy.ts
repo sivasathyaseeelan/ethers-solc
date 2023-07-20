b1cbec27a80c7b65d24a01f0f148dba15783e43d
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2023-07-20 15:17:13 +0530

    done

--- deploy.ts
+++ deploy.ts
@@ -6,14 +6,21 @@ async function main() {
   let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!)
   let wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
   const abi = fs.readFileSync("./Storage_sol_Storage.abi", "utf8")
-  const binary = fs.readFileSync(
-    "./Storage_sol_Storage.bin",
-    "utf8"
-  )
+  const binary = fs.readFileSync("./Storage_sol_Storage.bin", "utf8")
+
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
   console.log("Deploying, please wait...")
   const contract = await contractFactory.deploy()
-  console.log(contract)
+  const deploymentReceipt = await contract.deployTransaction.wait()
+  console.log(`Deployed to ${contract.address}`)
+
+  let currentFavoriteNumber = await contract.retrieve()
+  console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
+  console.log("Updating favorite number...")
+  let transactionResponse = await contract.store(10)
+  let transactionReceipt = await transactionResponse.wait()
+  currentFavoriteNumber = await contract.retrieve()
+  console.log(`New Favorite Number: ${currentFavoriteNumber}`)
 }
 
 main()
@@ -22,4 +29,3 @@ main()
     console.error(error)
     process.exit(1)
   })
-
