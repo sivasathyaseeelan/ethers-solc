62daeb92667e08f7674ff7f72d608927eaa3bcd1
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2023-07-20 11:47:38 +0530

    contract deployed

--- deploy.ts
+++ deploy.ts
@@ -1,10 +1,25 @@
-import { ethers } from "ethers";
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
-async function main() {}
+async function main() {
+  let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!)
+  let wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
+  const abi = fs.readFileSync("./Storage_sol_Storage.abi", "utf8")
+  const binary = fs.readFileSync(
+    "./Storage_sol_Storage.bin",
+    "utf8"
+  )
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Deploying, please wait...")
+  const contract = await contractFactory.deploy()
+  console.log(contract)
+}
 
 main()
-  .then(process.exit(0))
+  .then(() => process.exit(0))
   .catch((error) => {
-    console.log(error);
-    process.exit(0);
-  });
+    console.error(error)
+    process.exit(1)
+  })
+
